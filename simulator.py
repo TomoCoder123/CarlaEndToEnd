8bd709e898bce7c249725d0249d40ff655c1523a
TomoCoder123 <thomasxdeng@gmail.com>
2025-05-23 01:31:58 -0700

    Setting up spectator

---
+++ simulator.py
@@ -0,0 +1,19 @@
+import carla
+import random
+client = carla.Client('localhost')
+world = client.get_world()
+client.load_world('Town12') #or ./config.py --map Town05 
+
+# Retrieve the spectator object
+spectator = world.get_spectator()
+
+# Get the location and rotation of the spectator through its transform
+transform = spectator.get_transform()
+
+location = transform.location
+rotation = transform.rotation
+
+# Set the spectator with an empty transform
+spectator.set_transform(carla.Transform())
+# This will set the spectator at the origin of the map, with 0 degrees
+# pitch, yaw and roll - a good way to orient yourself in the map
